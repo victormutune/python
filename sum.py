1cf0be3acd9f0a2dcc71096adc77c3bd67b18db7
victormutune <victormutune254@gmail.com>
2025-04-04 20:15:16 +0300

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,12 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+# Perform the operation and display the result
+if operation == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+)
+else:
+    print("Invalid operation. Please use +")  
